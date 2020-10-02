dc1029fa2bd988891cab6f306c1075dead549e86
Aliaksei Trunou <trunof@gmail.com>
2020-10-02 21:34:59 +0300

    refactor: typical-array-problems solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+    var arr = [];
+    for(var i = 0; i < array.length; i++){
+      if(typeof array[i] === 'number'){
+        arr.push(array[i]);
+      }
+    }
+    var m = arr[0];
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] < m){
+          m = arr[i];
+        }
+      }
+      return m;
+  }else{
+    return 0;   
+  
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+    var arr = [];
+    for(var i = 0; i < array.length; i++){
+      if(typeof array[i] === 'number'){
+        arr.push(array[i]);
+      }
+    }
+    var m = arr[0];
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > m){
+          m = arr[i];
+        }
+      }
+      return m;
+  }else{
+    return 0;   
+  
+}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function min (array) {
+  if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+    var arr = [];
+    for(var i = 0; i < array.length; i++){
+      if(typeof array[i] === 'number'){
+        arr.push(array[i]);
+      }
+    }
+    var sumnum = 0;
+    for(var i = 0; i < arr.length; i++){
+      sumnum += arr[i];
+      }
+      return sumnum / arr.length;
+  }else{
+    return 0;   
+  
 }
+};
